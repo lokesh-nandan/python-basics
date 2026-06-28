56a7864329104a50424795c8eaba4ca1e052b445
Lokesh Nandan <nandan15lokesh@gmail.com>
2026-06-28 22:27:16 +0530

    checking rotation of a word

---
+++ problem_18.py
@@ -0,0 +1,7 @@
+a="waterbottle"
+b=a+a
+c=input("enter word to be tested: ")
+if c in b:
+    print("it is a rotation of the original word")
+else:
+    print("it is not a rotation of the original word")
